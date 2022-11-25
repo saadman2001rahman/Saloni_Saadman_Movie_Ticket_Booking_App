641bc960dce57e98e22ee337d0bf75b3b1cfbc91
saadman2001rahman <saadman.rahman1@ucalgary.ca>
2022-11-24 23:14:48 -0700

    added some of the scenes and implemented code to change scenes. Will need to add content to the scenes, as well as logic such as Movie class and User class, as well as login information for verification.

--- src/application/MovieTicketController.java
+++ src/application/MovieTicketController.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -21,29 +28,67 @@ public class MovieTicketController {
 
     @FXML
     private TextField login_page_password_field;
+        
+    @FXML 
+    private ChoiceBox movies_choicebox;
+    
+    @FXML
+    private Button rent_movies_button;
+    
+    @FXML 
+    ImageView imageView;
     
     @FXML
     void check_login(ActionEvent event) {
+    	/**
+    	 * Activates after login button is pressed in the first login scene. This will check whether username and password match with ones from record, and if they match, it will change the scene to one where there will be a list of movies to choose from.
+    	 */
     	String user_name = login_page_username_field.getText();
     	String password = login_page_password_field.getText();
     	boolean successful_login = true;
-    	
-//    	Scene rent_movie_scene = new Scene();
-//    	VBox temp_text_container = new VBox();
     	Label temp_label = new Label("This is just temporary. Teting whether the scene changes to a new scene from login page.");
-//    	temp_text_container.getChildren().addAll(temp_label);
     	Scene rent_movie_scene = new Scene(temp_label);
+
+    	try {
+    		FXMLLoader file_loader = new FXMLLoader();
+        	VBox rent_movie_container = file_loader.load(new FileInputStream("src/application/rent_movie_scene.fxml"));
+        	
+        	MovieTicketController acontroller = file_loader.getController();
+			acontroller.applicationStage = applicationStage;
+
+        	
+        	rent_movie_scene = new Scene(rent_movie_container);
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     	
     	if (successful_login) {
     		applicationStage.setScene(rent_movie_scene);
     	}
     }
     
-//    @FXML 
-//    void rent_movies(ActionEvent rentMovies) {
-//    	
-//    }
-}
+    @FXML
+    void movie_selected(ActionEvent user_selected_movie) {
+    	/**
+    	 * Activates after user chooses a movie to view, in which case it displays all information on the movie. Info includes movie title, genres, producer/author/director/actors, length, price of the movie, seating arrangement (***maybe another scene for seating arrangement??***)
+    	 */
+    	Label a_label = new Label("This is just temporary. Teting whether the scene changes to a new scene from login page.");
+    	Scene movie_info_scene = new Scene(a_label);
 
+    	try {
+    		FXMLLoader file_loader = new FXMLLoader();
+        	VBox movie_info_container = file_loader.load(new FileInputStream("src/application/movie_info_scene.fxml"));
+        	
+        	
+        	movie_info_scene = new Scene(movie_info_container);
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	applicationStage.setScene(movie_info_scene);
+    }
+}
 
 
