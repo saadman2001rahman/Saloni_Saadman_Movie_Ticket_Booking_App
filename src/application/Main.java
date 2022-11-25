a4dcff1544ccc5d9991564716becb96067e654b1
Saloni <Saloni@Salonis-Laptop>
2022-11-25 16:01:27 -0700

    Trying to see if we can pull changes from git into eclipse.

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("a title");
+			primaryStage.setTitle(" come watch a movie");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
