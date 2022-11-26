28ec5a527244bc69553288fafe6450cef1916e95
Saloni <Saloni@Salonis-Laptop.hitronhub.home>
2022-11-25 20:55:28 -0700

    Instance variable and methods were added to the seats class to categorize what a seat is and to eventually create a way to inform a user that a seat has been booked.

--- src/application/Seat.java
+++ src/application/Seat.java
@@ -2,4 +2,22 @@ package application;
 
 public class Seat {
 
+	private char row;
+	private int column;
+	
+	private Boolean bookingStatus = false;
+	
+	public Seat(char row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Boolean bookingStatus(Schedule date) {
+		return this.bookingStatus;
+		
+	}
+	
+	public void bookSeat() {
+		this.bookingStatus = true;
+	}
 }
