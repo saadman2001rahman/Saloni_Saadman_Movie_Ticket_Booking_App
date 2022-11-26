bba27949c399e49d50ab86faac0c7f2048592a8c
Saloni <Saloni@Salonis-Laptop.hitronhub.home>
2022-11-25 20:39:44 -0700

    Updates were made to the Schedule class to add different show timings.

--- src/application/Schedule.java
+++ src/application/Schedule.java
@@ -1,12 +1,18 @@
 package application;
 
 import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.Year;
 
 public class Schedule {
 	private Year year;
 	private Month month;
-	private DayOfWeek dayOfWeek;
+	private DayOfWeek day;
 	
+	private LocalTime[] time= {LocalTime.of(8, 45), LocalTime.of(10, 15), LocalTime.of(12, 15), LocalTime.of(14, 30), LocalTime.of(16, 10), LocalTime.of(19, 15), LocalTime.of(21, 30)};
+	
+	public Schedule(Year year, Month month, DayOfWeek day) {
+		
+	}
 }
