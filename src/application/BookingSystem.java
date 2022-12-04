6238c3b5833aedc185a683fcb5983639d434fb17
Saloni <Saloni@Salonis-Laptop>
2022-12-04 14:22:14 -0700

    Changes were made to booking system class to add a method that charges a certain amount for a certain theater. Schedule was updated to allow a schedule for movies to be created.

--- src/application/BookingSystem.java
+++ src/application/BookingSystem.java
@@ -1,11 +1,33 @@
 package application;
 
+import java.time.LocalTime;
+
 public class BookingSystem {
 	
 	
-	public void calculateTotalPrice(double basePrice) {
-		
-	}
+	public void calculateTotalPrice(double basePrice, Theater theater, Schedule date, Seat seatNum) {
+        double totalAmount = basePrice;
+        if(theater.getType() == "3D"){
+            totalAmount += 0.1 * totalAmount;
+        }
+        else if (theater.getType() == "Dolby") {
+            totalAmount += 0.2 * totalAmount;
+        }
+
+        if(date.getTime().isAfter(LocalTime.of(12,0))){
+            totalAmount += 0.1 * totalAmount;
+        }
+        else if (date.getTime().isAfter(LocalTime.of(6,0))){
+            totalAmount += 0.2 * totalAmount;
+        }
+
+        if(seatNum.getType() == "recliner"){
+            totalAmount += 0.2 * totalAmount;
+        }
+
+        return totalAmount;
+    }
+
 	
 	public void bookMovieTicket(Customer custName, Theater theater, Schedule date, Seat seatNumber) throws Exception{
 		if(!theater.seatBooked(date,  seatNumber)) {
--- src/application/Schedule.java
+++ src/application/Schedule.java
@@ -4,15 +4,30 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Schedule {
-	private Year year;
-	private Month month;
-	private DayOfWeek day;
+	private Date scheduledDate;
+	private LocalTime hourMin;
+	private ArrayList<Seat> availableSeats;
+	//for local time: https://www.joda.org/joda-time/apidocs/org/joda/time/LocalTime.html
 	
-	private LocalTime[] time= {LocalTime.of(8, 45), LocalTime.of(10, 15), LocalTime.of(12, 15), LocalTime.of(14, 30), LocalTime.of(16, 10), LocalTime.of(19, 15), LocalTime.of(21, 30)};
+	public Schedule(Date date, LocalTime hourMin) {
+		this.scheduledDate =date;
+		this.hourMin =hourMin;
+		this.scheduledDate.setHours(hourMin.getHour());
+		this.scheduledDate.setMinutes(hourMin.getMinute());
+		this.availableSeats = new ArrayList<>();
+		setAllSeats();	
+	}
 	
-	public Schedule(Year year, Month month, DayOfWeek day) {
+	public Schedule(int year, int month, int day, LocalTime hourMin) {
+		new Schedule(new Date (year, month, day), hourMin);
+	}
+
+	private void setAllSeats() {
 		
 	}
 }
