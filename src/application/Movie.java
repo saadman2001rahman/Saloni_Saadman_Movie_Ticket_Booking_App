fe6ef1856fae4719c7bb61aea173033189df3414
Saloni <Saloni@Salonis-Laptop>
2022-12-04 18:33:43 -0700

    Getter and Setter methods were added to movie class. Show class was deleted.

--- src/application/Movie.java
+++ src/application/Movie.java
@@ -1,22 +1,27 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class Movie extends Show {
+public class Movie {
 	
-	private String movieName;
+	private String movieName = "";
 	private final String[] movieGenre;
 	private int movieLengthMin;
+	private ArrayList<Theater> theater;
 	private double basePrice;
 	
-	public Movie (String mName, String[] mGenre, int mLen, double basePrice, Show show) {
-		super(show);
+	public Movie (String mName, String[] mGenre, int mLen, double basePrice) {
 		this.movieName = mName;
 		this.movieGenre = mGenre;
 		this.movieLengthMin = mLen;
 		this.basePrice= basePrice;
-		
+		this.theater = new ArrayList<>();
+	}
+	
+	public String getMovieName() {
+		return this.movieName;
 	}
 	
 	public Boolean movieGenreMatch(String[] movieGenre, String[] selectedGenre) {
@@ -47,6 +52,14 @@ public class Movie extends Show {
 
 	public double getbasePrice() {
 		return this.basePrice;
+	}
+	
+	public void setTheater(ArrayList<Theater> theater) {
+		this.theater = theater;
+	}
+	
+	public ArrayList<Theater> getTheaters() {
+		return this.theater;
 	}
 
 }
