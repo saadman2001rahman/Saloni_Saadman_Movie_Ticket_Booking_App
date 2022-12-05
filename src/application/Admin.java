fc190bc9771ea7053414eef3c353ce9fced3c7eb
Saloni <Saloni@10.14.161.183>
2022-12-05 12:35:03 -0700

    the admin class was updates to create a seating arrangement and a schedule.

--- src/application/Admin.java
+++ src/application/Admin.java
@@ -1,5 +1,63 @@
 package application;
 
-public class Admin {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Admin extends User{
+	public Admin(int uID, String uName, int uAge) {
+		super(uID, uName, uAge);
+	}
+	
+	public void addMovie(Movie movieToADD, Theater theaterNum, ArrayList<Schedule>availDates) {
+		theaterNum.setScheduleArrayList(availDates);
+		movieToAdd.addTheater(theaterNum);
+	}
+	
+	private static ArrayList<Seat> setAllSeats(){
+		ArrayList<Seat>availSeats = new ArrayList<>();
+		char[] rowArr = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+		int[] columnArr = {1,2,3,4,5,6,7,8,9,10,11};
+		for (char row : rowArr) {
+			for( int column : columnArr) {
+				if(row == 'E' && column >= 4 && <=8) {
+					availSeats.add(new Seat(row, column, "wheelchair"));
+				}
+				else if(row =='D' || row == 'C') {
+					availSeats.add(new Seat(row, column, "recliner"));
+				}
+				else {
+					availSeats.add(new Seat(row, column, "normal"));
+				}
+			}
+		}
+		return availSeats;
+	}
+	
+    private static Calendar getCalendarWithoutTime(Date date) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
+    }
+    
+    public static ArrayList<Date> getDatesBetween(Date startDate, Date endDate){
+    	ArrayList<Date> datesInRange = new ArrayList<>();
+    	Calendar calendar = getCalendarWithoutTime(startDate);
+    	Calendar endCalendar = getCalendarWithoutTime(endDate);
+    	
+    	while (calendar.before(endCalendar)) {
+    		Date result = calendar.getTime();
+    		datesInRange.add(result);
+    		calendar.add(Calendar.DATE, 1);
+    	}
+    	return datesInRange;
+    }
+	
 
 }
