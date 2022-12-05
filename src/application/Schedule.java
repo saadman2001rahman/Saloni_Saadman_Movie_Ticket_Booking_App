6918f7090072987c8a1f4f4030d830522e2ac62c
Saloni <Saloni@Salonis-Laptop>
2022-12-04 21:20:47 -0700

    Updated the Seat class to create a seat and allow it to check if a seat is booked in Schedule class.

--- src/application/Schedule.java
+++ src/application/Schedule.java
@@ -1,11 +1,7 @@
 package application;
 
-import java.time.DayOfWeek;
 import java.time.LocalTime;
-import java.time.Month;
-import java.time.Year;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Schedule {
@@ -14,15 +10,21 @@ public class Schedule {
 	private ArrayList<Seat> availableSeats;
 	//for local time: https://www.joda.org/joda-time/apidocs/org/joda/time/LocalTime.html
 	
-	public Schedule(Date date, LocalTime hourMin) {
+	public Schedule(Date date, LocalTime hourMin, ArrayList<Seat> availableSeats) {
 		this.scheduledDate =date;
 		this.hourMin =hourMin;
 		this.scheduledDate.setHours(hourMin.getHour());
 		this.scheduledDate.setMinutes(hourMin.getMinute());
-		this.availableSeats = new ArrayList<>();
-		setAllSeats();	
+		this.availableSeats = availableSeats;
 	}
-	
+	public Boolean checkBooking(Seat selectedSeat) {
+		for(Seat availSeat : this.availableSeats) {
+			if(availSeat.checkEqual(selectedSeat)) {
+				return true; //seat is available
+			}
+		}
+		return false; //seat is booked
+	}
 	public Schedule(int year, int month, int day, LocalTime hourMin) {
 		new Schedule(new Date (year, month, day), hourMin);
 	}
